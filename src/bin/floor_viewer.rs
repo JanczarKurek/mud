2ecfbe5a653717a897a8927b4de1b481febc2ba9
Janczar Knurek <janczar.knurek@doctoral.uj.edu.pl>
2026-04-26 00:20:59 +0200

    Change aliasing so tiles are rendered correctly

--- src/bin/floor_viewer.rs
+++ src/bin/floor_viewer.rs
@@ -3,14 +3,18 @@ use mud2::floor_viewer::plugin::FloorViewerPlugin;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: "Floor Viewer".into(),
-                resolution: (1280_u32, 800_u32).into(),
-                ..default()
-            }),
-            ..default()
-        }))
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Floor Viewer".into(),
+                        resolution: (1280_u32, 800_u32).into(),
+                        ..default()
+                    }),
+                    ..default()
+                }),
+        )
         .add_plugins(FloorViewerPlugin)
         .run();
 }
